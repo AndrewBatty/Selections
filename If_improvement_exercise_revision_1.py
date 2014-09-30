d2d33c8cd2643882d6bba1f8bb9704081af2b5d5
21553 <21553@longroad.ac.uk>
2014-09-30 15:20:46 +0100

    revision tasks for if exercises

---
+++ If_improvement_exercise_revision_1.py
@@ -0,0 +1,13 @@
+# Andrew Batty
+# Selection improvement ercise: Task 2
+# Revision exercise 1
+# 29/09/2014
+
+fristNumber = input("Please enter your frist number: ")
+secoundNumber = input("Please enter your secound number: ")
+
+if fristNumber == secoundNumber:
+    print("The numbers you have entered are the same.")
+else:
+    print("The numbers you have entered are not the same.")
+    
---
+++ If_improvement_exercise_revision_task2.py
@@ -0,0 +1,11 @@
+# Andrew Batty
+# Selection improvement ercise: Task 2
+# Revision exercise 2
+# 29/09/2014
+
+age = int(input("Please enter your age: "))
+
+if age >= 17:
+    print("You are old enogh to drive.")
+else:
+    print("You are not old enough to drive.")
