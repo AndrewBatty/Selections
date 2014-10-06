04de1cda8d1e19dd9ee9b09f77946c52d414ef10
Andrew Batty <21553@longroad.ac.uk>
2014-10-06 15:16:36 +0100

    final development exercise.

--- Selection_development_exercise_4.py
+++ Selection_development_exercise_4.py
@@ -4,11 +4,15 @@
 
 marks = int(input("Please enter the mark you got for the maths exam: "))
 
-if 0 > marks < 40:
-    print("The grade you got is a U.")
-elif 41 > marks < 50:
-    print("The grade you got is an E.")
-elif 51 > marks < 60:
-    print("The grade you got is a D.")
-elif 61 > marks < 70:
-    print("The grade you got is a C.")
+if 0 <= marks < 40:
+    print("You got a grade U.")
+elif 41 <= marks < 50:
+    print("You got a grade E.")
+elif 51 <= marks < 60:
+    print("You got a grade D.")
+elif 61 <= marks < 70:
+    print("You got a grade C.")
+elif 71 <= marks < 80:
+    print("You got a grade B.")
+elif 81 <= marks < 100:
+    print("You got a grade A.")
